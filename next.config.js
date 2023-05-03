5f373058e2fac792ddb29d0133ba986dd6d1931e
Dennis Frijlink <dennisfrijlink@gmail.com>
2023-05-03 21:48:27 +0200

    added pageExtensions to next config file

--- next.config.js
+++ next.config.js
@@ -2,44 +2,45 @@ const { i18n } = require("./next-i18next.config.js");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  i18n,
-  publicRuntimeConfig: {
-    apiURL: process.env.API_URL,
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*{/}?",
-        headers: [
-          {
-            key: "x-frame-options",
-            value: "deny",
-          },
-          {
-            key: "content-security-policy",
-            value: "frame-ancestors 'none'",
-          },
-          {
-            key: "x-content-type-options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "interest-cohort=()",
-          },
-        ],
-      },
-    ];
-  },
+	reactStrictMode: true,
+	i18n,
+	publicRuntimeConfig: {
+		apiURL: process.env.API_URL,
+	},
+	pageExtensions: ["api.ts", "page.tsx"],
+	async headers() {
+		return [
+			{
+				source: "/:path*{/}?",
+				headers: [
+					{
+						key: "x-frame-options",
+						value: "deny",
+					},
+					{
+						key: "content-security-policy",
+						value: "frame-ancestors 'none'",
+					},
+					{
+						key: "x-content-type-options",
+						value: "nosniff",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "same-origin",
+					},
+					{
+						key: "Strict-Transport-Security",
+						value: "max-age=31536000",
+					},
+					{
+						key: "Permissions-Policy",
+						value: "interest-cohort=()",
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
